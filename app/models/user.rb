22d80f9045b82d58db2796332b84f01b39310068
Anthony Breckner <skuunk@gmail.com>
2012-04-15 17:45:45 +1000

    Removing users scaffold as I am going to regenerate it using AuthLogic

--- app/models/user.rb
+++
@@ -1,4 +0,0 @@
-class User < ActiveRecord::Base
-  # attr_accessible :title, :body
-  has_many :recipes
-end
--- db/schema.rb
+++ db/schema.rb
@@ -54,10 +54,4 @@ ActiveRecord::Schema.define(:version => 20120415031042) do
     t.datetime "updated_at",          :null => false
   end
 
-  create_table "users", :force => true do |t|
-    t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
 end
