0bf71a1ff9ef299c196b92a4a4a82ef392a03fe8
Anthony Breckner <skuunk@gmail.com>
2012-05-06 19:43:57 +1000

    Refactoring link_tags to use map and join

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,13 +1,5 @@
 module RecipesHelper
   def link_tags(tags)
-    tag_links = ""
-    tags.each do |tag|
-      unless tag == tags.last
-        tag_links += "#{link_to tag.name, recipes_path(:tag => tag.name)}, "
-      else
-        tag_links += link_to tag.name, recipes_path(:tag => tag.name)
-      end
-    end
-    tag_links.html_safe
+    tags.map{|tag| link_to(tag.name, recipes_path(:tag => tag.name))}.join(', ').html_safe
   end
 end
