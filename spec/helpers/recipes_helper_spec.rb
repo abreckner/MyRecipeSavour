6d8dcb9a754e4c93e06c1e013b72f4d16d1d3ead
Anthony Breckner <skuunk@gmail.com>
2012-05-13 09:38:17 +1000

    updated test spec

--- spec/helpers/recipes_helper_spec.rb
+++ spec/helpers/recipes_helper_spec.rb
@@ -7,4 +7,11 @@ describe RecipesHelper do
     recipe.save
     helper.link_tags(recipe.tags).should == "<a href=\"/recipes?tag=tag3\">tag3</a>, <a href=\"/recipes?tag=tag2\">tag2</a>, <a href=\"/recipes?tag=tag1\">tag1</a>"
   end
+
+  it "should comma tags" do
+    recipe = Recipe.make
+    recipe.tag_list = "tag1, tag2, tag3"
+    recipe.save
+    helper.comma_tags(recipe.tags).should == "tag3, tag2, tag1"
+  end
 end
